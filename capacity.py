a7ad6d70a0936494f4b773866c0c591cba8489d0
dan.simone@oracle.com <dan.simone@oracle.com>
2018-02-14 17:42:04 -0800

    Refactor functions to better readability.

--- capacity.py
+++ capacity.py
@@ -4,11 +4,37 @@ import ConfigParser
 
 OCI_SECTION = 'OCI'
 
-def get_available_capacity(config_file):
+def get_region_availability_map(region, config, oci_config):
+    """
+    For the given region, returns a map of shape => available_per_ad_list
+    """
+    # Connect to OCI for the region
+    oci_config["region"] = region
+    oci.config.validate_config(oci_config)
+    cci = oci.core.compute_client.ComputeClient(oci_config)
+    identity = oci.identity.IdentityClient(oci_config)
 
+    # Initialize available capacity map for each provided shape, for each AD
+    region_availability_map = {}
+    for (shape, capacity_per_ad) in config.items(region):
+        int_per_ad = int(capacity_per_ad)
+        region_availability_map[shape] = [int_per_ad, int_per_ad, int_per_ad]
+
+    # Loop through all compartments and count running compute instances
+    for compartment in identity.list_compartments(compartment_id=oci_config['tenancy']).data:
+        for instance in cci.list_instances(compartment_id=compartment.id).data:
+            if instance.lifecycle_state == 'RUNNING' and instance.shape in region_availability_map:
+                ad = int(instance.availability_domain.split('-')[-1])
+                region_availability_map[instance.shape][ad - 1] -= 1
+    return region_availability_map
+
+def get_total_availability_map(config_file):
+    """
+    Returns a map of region -> region_availability_map
+    """
     # Parse config file for OCI credentials
     config = ConfigParser.ConfigParser()
-    config.optionxform=str
+    config.optionxform=str # makes case sensitive
     config.read(config_file)
     oci_config = {}
     oci_config["user"] = config.get(OCI_SECTION, 'user')
@@ -16,46 +42,27 @@ def get_available_capacity(config_file):
     oci_config["key_file"] = config.get(OCI_SECTION, 'key_file')
     oci_config["fingerprint"] = config.get(OCI_SECTION, 'fingerprint')
 
-    available_capacity = {}
-
     # Parse available regions from config file
     regions = config.sections()
     regions.remove(OCI_SECTION)
 
     # Determine capacity per region
+    total_availability_map = {}
     for region in regions:
-        # Connect to OCI for the region
-        oci_config["region"] = region
-        oci.config.validate_config(oci_config)
-        cci = oci.core.compute_client.ComputeClient(oci_config)
-        identity = oci.identity.IdentityClient(oci_config)
-
-        # Initialize available capacity map for each provided shape, for each AD
-        region_available_capacity = {}
-        available_capacity[region] = region_available_capacity
-        for (shape, number_per_ad) in config.items(region):
-            int_per_ad = int(number_per_ad)
-            region_available_capacity[shape] = [int_per_ad, int_per_ad, int_per_ad]
-
-        # Loop through all compartments and count running compute instances
-        for compartment in identity.list_compartments(compartment_id=oci_config['tenancy']).data:
-            for instance in cci.list_instances(compartment_id=compartment.id).data:
-                if instance.lifecycle_state == 'RUNNING' and instance.shape in region_available_capacity:
-                    ad = int(instance.availability_domain.split('-')[-1])
-                    region_available_capacity[instance.shape][ad - 1] -= 1
-    return available_capacity
+        total_availability_map[region] = get_region_availability_map(region, config=config, oci_config=oci_config)
+    return total_availability_map
 
 # Check parameters
 parser = argparse.ArgumentParser(description='List available compute capacity in OCI')
-parser.add_argument('--config', type=str, help='OCI config file')
+parser.add_argument('--config', type=str, help='Config file')
 args = parser.parse_args()
 
-available_capacity = get_available_capacity(args.config)
-for region in available_capacity:
+total_availability_map = get_total_availability_map(args.config)
+for region in total_availability_map:
     print '************************************************'
     print 'Available shapes in %s' % region
     print '************************************************'
-    for shape in available_capacity[region]:
-        ad_capacity_list = available_capacity[region][shape]
+    for shape in total_availability_map[region]:
+        ad_capacity_list = total_availability_map[region][shape]
         if sum(ad_capacity_list) > 0:
             print '%s: AD1: %s, AD2: %s, AD3: %s' % (shape, ad_capacity_list[0], ad_capacity_list[1], ad_capacity_list[2])
